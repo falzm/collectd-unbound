9ff30d1cc9a86b4a135dcfac9872274af67460db
Marc Falzon <marc@baha.mu>
2017-06-20 09:58:12 +0200

    Fix metric DS types

--- collectd-unbound.go
+++ collectd-unbound.go
@@ -14,6 +14,25 @@ import (
 	"collectd.org/exec"
 )
 
+var (
+	metrics = map[string]string{
+		"num_queries":              "derive",
+		"num_cachehits":            "derive",
+		"num_cachemiss":            "derive",
+		"num_prefetch":             "derive",
+		"num_zero_ttl":             "derive",
+		"num_recursivereplies":     "derive",
+		"requestlist_avg":          "gauge",
+		"requestlist_max":          "derive",
+		"requestlist_overwritten":  "derive",
+		"requestlist_exceeded":     "derive",
+		"requestlist_current_all":  "derive",
+		"requestlist_current_user": "derive",
+		"recursion_time_avg":       "gauge",
+		"recursion_time_median":    "gauge",
+	}
+)
+
 func main() {
 	e := exec.NewExecutor()
 	e.VoidCallback(unboundStats, exec.Interval())
@@ -21,6 +40,8 @@ func main() {
 }
 
 func unboundStats(ctx context.Context, interval time.Duration) {
+	var vl *api.ValueList
+
 	buf := &bytes.Buffer{}
 	cmd := osExec.Command("/bin/sh", "-c", "unbound-control stats_noreset")
 	cmd.Stdout = buf
@@ -47,22 +68,42 @@ func unboundStats(ctx context.Context, interval time.Duration) {
 		metric = strings.TrimPrefix(metric, "total.")
 		metric = strings.Replace(metric, ".", "_", -1)
 
+		if _, ok := metrics[metric]; !ok {
+			continue
+		}
+
 		value, err := strconv.ParseFloat(fields[1], 64)
 		if err != nil {
 			log.Printf("error: unable to parse metric value: %v", err)
 			continue
 		}
 
-		vl := &api.ValueList{
-			Identifier: api.Identifier{
-				Host:         exec.Hostname(),
-				Plugin:       "unbound",
-				Type:         "gauge",
-				TypeInstance: metric,
-			},
-			Time:     now,
-			Interval: interval,
-			Values:   []api.Value{api.Gauge(value)},
+		switch metrics[metric] {
+		case "derive":
+			vl = &api.ValueList{
+				Identifier: api.Identifier{
+					Host:         exec.Hostname(),
+					Plugin:       "unbound",
+					Type:         "derive",
+					TypeInstance: metric,
+				},
+				Time:     now,
+				Interval: interval,
+				Values:   []api.Value{api.Derive(value)},
+			}
+
+		case "gauge":
+			vl = &api.ValueList{
+				Identifier: api.Identifier{
+					Host:         exec.Hostname(),
+					Plugin:       "unbound",
+					Type:         "gauge",
+					TypeInstance: metric,
+				},
+				Time:     now,
+				Interval: interval,
+				Values:   []api.Value{api.Gauge(value)},
+			}
 		}
 
 		exec.Putval.Write(ctx, vl)
